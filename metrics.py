0dd13254787a3bf29905df693dde37bc4578c366
freej <jenkoslobodan@gmail.com>
2020-04-21 14:41:48 +0200

    Added MSE, R2 and Linear regression. Gradien descent not working

---
+++ metrics.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+def MSE(model, X, y):
+    y_pred = model.predict(X)
+    return np.mean((y_pred - y)**2)
+
+def R2(model, X, y):
+    return 1 - MSE(model, X, y) / np.var(y)
+
+#Tests functions
+if __name__ == "__main__":
+    #Preparation
+    from sklearn.linear_model import LinearRegression
+    from sklearn.linear_model import LogisticRegression
+    from sklearn.datasets import load_iris
+    from sklearn.datasets import load_boston
+
+    cModel = LogisticRegression()
+    iris = load_iris()
+    cX = iris.data
+    cy = iris.target
+    cModel.fit(cX, cy)
+
+    rModel = LinearRegression()
+    boston = load_boston()
+    rX = boston.data
+    ry = boston.target
+    rModel.fit(rX, ry)
+
+    #Testing
+    from sklearn.metrics import mean_squared_error
+    if(MSE(rModel, rX, ry) == mean_squared_error(ry, rModel.predict(rX))):
+        print('MSE: VALID')
+    else:
+        print('MSE: ERROR')
+
+
+    from sklearn.metrics import r2_score
+    if(R2(rModel, rX, ry) == r2_score(ry, rModel.predict(rX))):
+        print('R2: VALID')
+    else:
+        print('R2: ERROR')
+
